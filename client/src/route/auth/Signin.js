10836a5391a60ad14480f2900cbfa429b8d93d89
KarstenMalle <Karsten_malle@hotmail.com>
2023-02-21 11:46:58 +0100

    gitlab graphql API

--- client/src/route/auth/Signin.js
+++ client/src/route/auth/Signin.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { useEffect } from 'react';
 
 // const REDIRECT_URI = 'http://localhost:4000/dashboard';
@@ -13,9 +14,37 @@ function App() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const codeParam = urlParams.get('code');
-    if (codeParam && (localStorage.getItem('accessToken') === null)) {
+    async function fetchGitLabGraphQLQuery(accessToken, query) {
+      try {
+        const response = await fetch('https://gitlab.com/api/graphql', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            // eslint-disable-next-line quote-props
+            Authorization: `Bearer ${accessToken}`,
+          },
+          body: JSON.stringify({ query }),
+        });
+        if (!response.ok) {
+          throw new Error(
+            `Error fetching GitLab GraphQL data: ${response.statusText}`,
+          );
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    }
+    if (codeParam && localStorage.getItem('accessToken') === null) {
       // eslint-disable-next-line no-inner-declarations
-      async function getGitLabAccessToken(clientId, clientSecret, redirectUri, authCode) {
+      async function getGitLabAccessToken(
+        clientId,
+        clientSecret,
+        redirectUri,
+        authCode,
+      ) {
         try {
           const response = await fetch('https://gitlab.com/oauth/token', {
             method: 'POST',
@@ -24,15 +53,17 @@ function App() {
             },
             body: new URLSearchParams({
               grant_type: 'authorization_code',
-              client_id: CLIENT_ID,
-              client_secret: CLIENT_SECRET,
-              redirect_uri: REDIRECT_URI,
+              client_id: clientId,
+              client_secret: clientSecret,
+              redirect_uri: redirectUri,
               code: authCode,
             }),
           });
 
           if (!response.ok) {
-            throw new Error(`Error fetching access token: ${response.statusText}`);
+            throw new Error(
+              `Error fetching access token: ${response.statusText}`,
+            );
           }
 
           const data = await response.json();
@@ -46,23 +77,69 @@ function App() {
       }
       try {
         // eslint-disable-next-line max-len
-        const accessToken = await getGitLabAccessToken(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, codeParam);
+        const accessToken = await getGitLabAccessToken(
+          CLIENT_ID,
+          CLIENT_SECRET,
+          REDIRECT_URI,
+          codeParam,
+        );
+        localStorage.setItem('accessToken', accessToken);
+        const query = `
+          {
+            viewer {
+              username
+            }
+          }
+        `;
+
+        const data = await fetchGitLabGraphQLQuery(accessToken, query);
+
+        if (data.errors) {
+          data.errors.forEach((error) => {
+            console.error(error.message);
+          });
+        } else {
+          console.log(data);
+        }
         console.log(`Access token: ${accessToken}`);
       } catch (error) {
         console.error(`Error fetching access token: ${error}`);
       }
+    } else if (localStorage.getItem('accessToken')) {
+      const accessToken = localStorage.getItem(('accessToken'));
+      console.log('TOKEN IS: ', accessToken);
+      const query = `
+          {
+            currentUser {
+              id
+              name
+              username
+              email
+            }
+          }
+        `;
+
+      const data = await fetchGitLabGraphQLQuery(accessToken, query);
+
+      if (data.errors) {
+        data.errors.forEach((error) => {
+          console.error(error.message);
+        });
+      } else {
+        console.log(data.currentUser);
+      }
     }
   }, []);
   function loginWithGitlab() {
-    window.location.assign(`https://gitlab.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${REQUESTED_SCOPES}`);
+    window.location.assign(
+      `https://gitlab.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${REQUESTED_SCOPES}`,
+    );
   }
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <button onClick={loginWithGitlab}>
-          Login with Gitlab
-        </button>
+    <div className="App">
+      <header className="App-header">
+        <button onClick={loginWithGitlab}>Login with Gitlab</button>
       </header>
     </div>
   );
